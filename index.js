fa64439cf3125594cbc019ee51dd4478056b89ab
manihateu <102178609+manihateu@users.noreply.github.com>
2023-05-04 20:33:10 +0300

    PORT

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Product = require('./Models/Product');
 const bcrypt = require('bcrypt');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const secretKey = 'nik852';
 
 app.use(bodyParser.urlencoded({ extended: false }));
